76dbcc0ae12fe6900adf45762d889d63548894f6
形山竜一 <ktymryich2322.developer@gmail.com>
2022-04-25 15:41:54 +0900

    add: npmでvuetifyをインストール

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Reserve from "../views/Reserve.vue";
+import Today from "../views/Today.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/status",
+        name: "Status",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/Status.vue"),
+    },
+    {
+        path: "/reserve",
+        name: "Reserve",
+        component: Reserve,
+    },
+    {
+        path: "/today",
+        name: "Today",
+        component: Today,
+    },
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
 
-export default router
+export default router;
