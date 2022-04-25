3974ece600dc8c61db826dfa986ceb359f20331c
形山竜一 <ktymryich2322.developer@gmail.com>
2022-04-25 22:22:24 +0900

    add: 予約状況確認ページいい感じ

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,12 +1,48 @@
 <template>
     <div class="status">
-        <v-container>
-            <v-row>
-                <h1>This is an status page</h1>
+        <v-container fluid>
+            <v-row dense justify="space-between">
+                <v-col cols="12" style="text-align: center">
+                    <h1>Rserving status</h1>
+                </v-col>
             </v-row>
-            <!-- <v-row> -->
+
+            <v-row dense>
+                <v-col
+                    v-for="card in cards"
+                    :key="card.title"
+                    :cols="card.flex"
+                >
+                    <v-card elevation="0" class="mb-4" tile>
+                        <v-img
+                            src="../assets/2681826 1.png"
+                            class="white--text align-end rounded-lg"
+                            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                            height="200px"
+                        >
+                            <v-card-title>
+                                {{ card.title }}
+                                <v-spacer></v-spacer>
+                                <v-icon class="" style="color: white">
+                                    mdi-white-balance-sunny
+                                </v-icon>
+                                <v-icon style="color: white">
+                                    mdi-moon-waxing-crescent
+                                </v-icon>
+                            </v-card-title>
+                        </v-img>
+                    </v-card>
+                </v-col>
+            </v-row>
             <UnderMenu />
-            <!-- </v-row> -->
+
+            <v-row>
+                <v-col cols="12" class="mt-16" style="color: white">
+                    <h1 class="white">
+                        ココ見つけたら連絡してよ。リクルートだよ
+                    </h1>
+                </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
@@ -19,5 +55,59 @@ export default {
     components: {
         UnderMenu,
     },
+    data: () => ({
+        cards: [
+            {
+                title: "4/25 Mon",
+                src: "../assets/2681826 1.png",
+                flex: 12,
+            },
+            {
+                title: "4/26 Tue",
+                src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+                flex: 6,
+            },
+            {
+                title: "4/27 Wed",
+                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+                flex: 6,
+            },
+            {
+                title: "4/27 Wed",
+                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+                flex: 6,
+            },
+            {
+                title: "4/27 Wed",
+                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+                flex: 6,
+            },
+            {
+                title: "4/27 Wed",
+                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+                flex: 6,
+            },
+            {
+                title: "4/27 Wed",
+                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+                flex: 6,
+            },
+            {
+                title: "4/27 Wed",
+                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+                flex: 6,
+            },
+            {
+                title: "4/27 Wed",
+                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+                flex: 6,
+            },
+            {
+                title: "4/27 Wed",
+                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+                flex: 6,
+            },
+        ],
+    }),
 };
 </script>
--- src/views/Today.vue
+++ src/views/Today.vue
@@ -1,23 +1,69 @@
 <template>
-    <div class="today">
-        <v-container>
-            <v-row>
-                <h1>This is a Today page</h1>
-            </v-row>
-            <v-row>
-                <UnderMenu />
+    <v-card class="mx-auto" max-width="500">
+        <v-container fluid>
+            <v-row dense>
+                <v-col
+                    v-for="card in cards"
+                    :key="card.title"
+                    :cols="card.flex"
+                >
+                    <v-card>
+                        <v-img
+                            :src="card.src"
+                            class="white--text align-end"
+                            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                            height="200px"
+                        >
+                            <v-card-title v-text="card.title"></v-card-title>
+                        </v-img>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+
+                            <v-btn icon>
+                                <v-icon>mdi-heart</v-icon>
+                            </v-btn>
+
+                            <v-btn icon>
+                                <v-icon>mdi-bookmark</v-icon>
+                            </v-btn>
+
+                            <v-btn icon>
+                                <v-icon>mdi-share-variant</v-icon>
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
             </v-row>
         </v-container>
-    </div>
+    </v-card>
 </template>
-
 <script>
-import UnderMenu from "@/components/UnderMenu.vue";
+// import UnderMenu from "@/components/UnderMenu.vue";
 
 export default {
     name: "Home",
     components: {
-        UnderMenu,
+        // UnderMenu,
     },
+    data: () => ({
+        cards: [
+            {
+                title: "Pre-fab homes",
+                src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+                flex: 12,
+            },
+            {
+                title: "Favorite road trips",
+                src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+                flex: 6,
+            },
+            {
+                title: "Best airlines",
+                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+                flex: 6,
+            },
+        ],
+    }),
 };
 </script>
