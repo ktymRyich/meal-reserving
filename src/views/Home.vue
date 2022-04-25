67ca1272995f7c4011f4fa83c8fc2db043245f9d
形山竜一 <ktymryich2322.developer@gmail.com>
2022-04-25 15:43:05 +0900

    add: これデフォルトのやつだいらないかも

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <UnderMenu />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import UnderMenu from "@/components/UnderMenu.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "Home",
+    components: {
+        HelloWorld,
+        UnderMenu,
+    },
+};
 </script>
