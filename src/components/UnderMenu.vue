0892e56b1f9f5570bde13af3b41b97110c2bd41b
形山竜一 <ktymryich2322.developer@gmail.com>
2022-04-25 15:42:39 +0900

    add: ページ下部のメニューを作成

---
+++ src/components/UnderMenu.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-bottom-navigation fixed color="primary">
+        <v-btn to="/reserve">
+            <!-- <span> Reserve </span> -->
+            <v-icon>mdi-history</v-icon>
+        </v-btn>
+
+        <v-btn to="/status">
+            <!-- <span> Status </span> -->
+            <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn to="/today">
+            <!-- <span>Nearby</span> -->
+            <v-icon>mdi-map-marker</v-icon>
+        </v-btn>
+    </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+    name: "UnderMenu",
+};
+</script>
