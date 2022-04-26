de2affeddb25a4d9202e7106a04923aa936ec558
形山竜一 <ktymryich2322.developer@gmail.com>
2022-04-27 01:48:06 +0900

    change: 最下段のアイコンを変更

--- src/components/UnderMenu.vue
+++ src/components/UnderMenu.vue
@@ -2,17 +2,17 @@
     <v-bottom-navigation fixed color="primary">
         <v-btn to="/reserve">
             <!-- <span> Reserve </span> -->
-            <v-icon>mdi-history</v-icon>
+            <v-icon>mdi-rice</v-icon>
         </v-btn>
 
         <v-btn to="/status">
             <!-- <span> Status </span> -->
-            <v-icon>mdi-heart</v-icon>
+            <v-icon>mdi-calendar-check</v-icon>
         </v-btn>
 
         <v-btn to="/today">
             <!-- <span>Nearby</span> -->
-            <v-icon>mdi-map-marker</v-icon>
+            <v-icon>mdi-account-group</v-icon>
         </v-btn>
     </v-bottom-navigation>
 </template>
