6791f53d756d96c1261c3fb8c6a178d61ed74005
形山竜一 <ktymryich2322.developer@gmail.com>
2022-05-16 15:47:35 +0900

    add: データベースの取得文を追記。ただいらないかも

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+
 // Import the functions you need from the SDKs you need
 import {
     getAuth,
@@ -11,8 +12,11 @@ import {
 } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase, child, ref, get } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+Vue.config.productionTip = false;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -30,29 +34,52 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 getAnalytics(app);
 
-Vue.config.productionTip = false;
-
 const provider = new GoogleAuthProvider();
-
 const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        // const uid = user.uid;
-        // ...
-        console.log("This user is already logined");
-    } else {
-        // User is signed out
-        // ...
-        console.log("Get auth");
-        signInWithRedirect(auth, provider);
-    }
-});
 
+if (1 === auth) {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // console.log("This user is already logined");
+            user.getIdToken().then(() => {
+                console.log("The token was got");
+            });
+            // user.providerData.forEach((profile) => {
+            //     console.log("Sign-in provider: " + profile.providerId);
+            //     console.log("  Provider-specific UID: " + profile.uid);
+            //     console.log("  Name: " + profile.displayName);
+            //     console.log("  Email: " + profile.email);
+            //     console.log("  Photo URL: " + profile.photoURL);
+            // });
+            // console.log("User Display Name : " + user.displayName);
+        } else {
+            console.log("Get auth");
+            signInWithRedirect(auth, provider);
+        }
+    });
+}
+
+// データベース取得
+const database = getDatabase();
+const dbRef = ref(database);
+// メニューの取得
+let menu;
+if (1 === menu) {
+    get(child(dbRef, `Menu/`))
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                menu = snapshot.val();
+                console.log(menu);
+            } else {
+                console.log("No data available");
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
 new Vue({
     router,
     vuetify,
