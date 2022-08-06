1b960a0acfeb81c96c389e20ec71a5bdfe6f4a2e
形山竜一 <ktymryich2322.developer@gmail.com>
2022-08-06 20:15:25 +0900

    add: editorページの作成

---
+++ src/views/Editor.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="editor">
+        <v-container>
+            <v-row>
+                <v-col cols="12" style="text-align: center">
+                    <h1>{{ mealData.date }}</h1>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col cols="12">
+                    <v-file-input
+                        accept="image/*"
+                        label="Meal Image"
+                        prepend-icon="mdi-camera"
+                        :v-model="mealData.img"
+                    ></v-file-input>
+                    <v-img
+                        lazy-src="https://picsum.photos/id/11/10/6"
+                        height="150"
+                        width="100%"
+                        :src="mealData.img"
+                    ></v-img>
+                </v-col>
+                <v-col cols="12">
+                    <h2>{{ mealData.name }}</h2>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// import {
+//     getAuth,
+//     signInWithRedirect,
+//     GoogleAuthProvider,
+//     onAuthStateChanged,
+// } from "firebase/auth";
+import { getDatabase, ref, get, child } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtgBKlIY_tBn003irV44d9Qu54Kvt_dYo",
+    authDomain: "meal-reserving-system.firebaseapp.com",
+    databaseURL:
+        "https://meal-reserving-system-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "meal-reserving-system",
+    storageBucket: "meal-reserving-system.appspot.com",
+    messagingSenderId: "1043572372155",
+    appId: "1:1043572372155:web:0584b2959e3f6561782f9e",
+    measurementId: "G-8BRYQ6B2HD",
+};
+
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
+// const auth = getAuth();
+// const provider = new GoogleAuthProvider();
+const db = getDatabase();
+const dbRef = ref(db);
+
+export default {
+    name: "Editor",
+    components: {},
+    data: () => ({
+        mealData: {
+            name: "",
+            date: "0",
+            img: "",
+        },
+    }),
+    methods: {},
+    watch: {
+        mealData: function () {
+            console.log(this.mealData.img);
+        },
+    },
+    created: function () {},
+    mounted: function () {
+        // メニュー取得
+        let mealID = parseInt(this.$route.query.mealID);
+        let mealDay = Math.floor(mealID / 10);
+        let mealTime = mealID % 10 == 0 ? "morning" : "night";
+        let requestUrl = `Menu/` + mealTime + `/` + mealDay;
+
+        this.mealData.date =
+            String(parseInt(mealDay / 10000)) +
+            "." +
+            String(parseInt((mealDay % 10000) / 100)) +
+            "." +
+            String(parseInt(mealDay % 100));
+
+        console.log(requestUrl);
+        get(child(dbRef, requestUrl)).then((snapshot) => {
+            if (snapshot.exists()) {
+                let meal = snapshot.val();
+                this.mealData.name = meal.mealname;
+                this.mealData.img = meal.img;
+                console.log(meal);
+            } else {
+                console.log("Not exist");
+                this.mealData.name = "inputhere";
+                this.mealData.img = "https://picsum.photos/id/11/10/6";
+            }
+        });
+    },
+};
+</script>
