3ef037de209fee65056aaf253e663a69f851d564
形山竜一 <ktymryich2322.developer@gmail.com>
2022-08-06 20:15:58 +0900

    add: 料理登録一覧ページを追加

--- src/views/Today.vue
+++ src/views/Today.vue
@@ -1,42 +1,278 @@
 <template>
-    <div class="today">
+    <div class="mealregister">
         <v-container>
             <v-row>
-                <v-col>
-                    <h1>This is a Today page</h1>
-                    <p>This page is under development!</p>
+                <v-col cols="12" style="text-align: center">
+                    <h1>Meal Reserving</h1>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col cols="12">
+                    <h2>Breakfasts</h2>
+                </v-col>
+            </v-row>
+            <v-row no-gutters>
+                <v-col cols="12">
+                    <v-slide-group multiple>
+                        <v-slide-item v-for="(meal, i) in menuMorning" :key="i">
+                            <v-col>
+                                <v-card width="140" rounded="lg" elevation="0">
+                                    <v-img
+                                        src="../assets/2681826 1.png"
+                                        class="rounded-lg"
+                                        height="180"
+                                    ></v-img>
+                                    <div class="my-2">
+                                        <h4>{{ meal.mealName }}</h4>
+                                        {{
+                                            ((meal.date / 100).toFixed() %
+                                                100) +
+                                            "/" +
+                                            (meal.date % 100)
+                                        }}
+                                    </div>
+                                    <v-btn
+                                        active-class="grey darken-4 white--text"
+                                        depressed
+                                        to="/?a=3"
+                                    >
+                                        edit
+                                    </v-btn>
+                                </v-card>
+                            </v-col>
+                        </v-slide-item>
+                    </v-slide-group>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col cols="12">
+                    <h2>Diners</h2>
+                </v-col>
+            </v-row>
+            <v-row no-gutters>
+                <v-col cols="12">
+                    <v-slide-group multiple>
+                        <v-slide-item
+                            v-for="meal in menuNight"
+                            :key="meal.date"
+                        >
+                            <v-col>
+                                <v-card width="140" rounded="lg" elevation="0">
+                                    <v-img
+                                        src="../assets/2681826 1.png"
+                                        class="rounded-lg"
+                                    ></v-img>
+                                    <div class="my-2">
+                                        <h4>
+                                            {{ meal.mealName }}
+                                        </h4>
+                                        {{
+                                            ((meal.date / 100).toFixed() %
+                                                100) +
+                                            "/" +
+                                            (meal.date % 100)
+                                        }}
+                                    </div>
+                                </v-card>
+                            </v-col>
+                        </v-slide-item>
+                    </v-slide-group>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="12" class="mt-16" style="color: white">
+                    <h1 class="white">ココ見つけたら連絡してよ</h1>
                 </v-col>
             </v-row>
             <UnderMenu />
         </v-container>
     </div>
 </template>
+
 <script>
 import UnderMenu from "@/components/UnderMenu.vue";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+    getAuth,
+    signInWithRedirect,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+} from "firebase/auth";
+import { getDatabase, ref, get, child, update } from "firebase/database";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDtgBKlIY_tBn003irV44d9Qu54Kvt_dYo",
+    authDomain: "meal-reserving-system.firebaseapp.com",
+    databaseURL:
+        "https://meal-reserving-system-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "meal-reserving-system",
+    storageBucket: "meal-reserving-system.appspot.com",
+    messagingSenderId: "1043572372155",
+    appId: "1:1043572372155:web:0584b2959e3f6561782f9e",
+    measurementId: "G-8BRYQ6B2HD",
+};
+
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+const db = getDatabase();
+const dbRef = ref(db);
+
+let getDateString = function (d) {
+    return (
+        "" +
+        d.getFullYear() +
+        ("00" + (d.getMonth() + 1)).slice(-2) +
+        ("00" + d.getDate()).slice(-2)
+    );
+};
+
+let menu;
 export default {
-    name: "Home",
+    name: "MealRegistration",
     components: {
         UnderMenu,
     },
     data: () => ({
-        cards: [
-            {
-                title: "Pre-fab homes",
-                src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-                flex: 12,
-            },
-            {
-                title: "Favorite road trips",
-                src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
-                flex: 6,
-            },
-            {
-                title: "Best airlines",
-                src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
-                flex: 6,
-            },
-        ],
+        menuMorning: {},
+        menuNight: {},
+        res: "",
+        uid: null,
+        today: null,
     }),
+    methods: {
+        abc() {
+            alert("abc");
+        },
+        userChange(m) {
+            console.log(
+                "User changed the reserving states : " +
+                    (m === 0 ? "morning" : "night")
+            );
+            this.isChanged = true;
+        },
+        // ============================== 予約状況のアップロード ==============================
+        updateReservation() {
+            // ボタンのモード変更
+            this.isChanged = false;
+            this.updating = true;
+
+            console.log("model morning: ");
+            console.log(this.modelMorning);
+            console.log("model Night: ");
+
+            console.log(this.modelNight);
+            this.reserving = {
+                morning: {},
+                night: {},
+            };
+            for (let i = 0; i < Object.keys(this.modelMorning).length; i++) {
+                let date =
+                    "" +
+                    this.menuMorning[
+                        Object.keys(this.menuMorning)[this.modelMorning[i]]
+                    ].date;
+                this.reserving.morning[String(date)] = true;
+            }
+            for (let i = 0; i < Object.keys(this.modelNight).length; i++) {
+                let date =
+                    "" +
+                    this.menuNight[
+                        Object.keys(this.menuNight)[this.modelNight[i]]
+                    ].date;
+                this.reserving.night[String(date)] = true;
+            }
+            console.log(this.reserving);
+
+            update(ref(db, "Users/" + this.uid + "/"), {
+                reserving: this.reserving,
+            })
+                .then(() => {
+                    this.updating = false;
+                })
+                .catch((err) => {
+                    alert(err);
+                });
+        },
+        getDate(n) {
+            let d = new Date();
+            d.setDate(d.getDate() + n);
+            return getDateString(d);
+        },
+    },
+    watch: {
+        modelMorning: function () {},
+        modelNight: function () {},
+    },
+    created: function () {
+        console.log(this.$route.query.mealId);
+        // 今日の日付の取得
+        let d = new Date();
+        this.today = getDateString(d);
+
+        this.modelMorning = [];
+
+        // ユーザー認証処理
+        onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                signInWithRedirect(auth, provider);
+            } else {
+                this.uid = user.uid;
+            }
+        });
+    },
+    mounted: function () {
+        // メニュー取得
+        get(child(dbRef, `Menu/`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                menu = snapshot.val();
+
+                let day = new Date();
+                let m = {};
+                let n = {};
+                for (let i = 0; i < 7; i++) {
+                    let d = getDateString(day);
+                    // 朝ご飯のメニュー反映
+                    m[parseInt(d)] =
+                        parseInt(d) in menu.morning
+                            ? {
+                                  date: parseInt(d),
+                                  img: menu.morning[parseInt(d)].img,
+                                  mealName: menu.morning[parseInt(d)].mealName,
+                              }
+                            : {
+                                  date: parseInt(d),
+                                  img: 0,
+                                  mealName: "no info",
+                              };
+                    // 夜ご飯のメニュー反映
+                    n[parseInt(d)] =
+                        parseInt(d) in menu.night
+                            ? {
+                                  date: parseInt(d),
+                                  img: menu.night[parseInt(d)].img,
+                                  mealName: menu.night[parseInt(d)].mealName,
+                              }
+                            : {
+                                  date: parseInt(d),
+                                  img: 0,
+                                  mealName: "no info",
+                              };
+                    day.setDate(day.getDate() + 1);
+                }
+                // vue内の変数に反映
+                this.menuMorning = m;
+                this.menuNight = n;
+            } else {
+                console.log("Database get error @Menu");
+                menu = {};
+            }
+        });
+    },
 };
 </script>
